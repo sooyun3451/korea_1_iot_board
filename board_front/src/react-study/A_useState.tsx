f031c42c364894c599858c3a0374ec8eb9a25d21
sooyun3451 <sooyun020820@gmail.com>
2024-10-28 12:49:20 +0900

    20241028

--- board_front/src/react-study/A_useState.tsx
+++ board_front/src/react-study/A_useState.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // ! useState
 // 1. 함수형 컴포넌트
@@ -22,19 +22,32 @@ import React, { useState } from 'react'
 // cf) 구조 분해 할당: 배열이나 객체의 속성을 변수로 쉽게 추출할 수 있도록 하는 기능
 
 // 배열
-const [a, b] = [1, 2];
+// const [a, b] = [1, 2];
 
-// 객체 
-const { name, age } = {
-  name: '홍길동',
-  age: 30
-};
+// 객체
+// const { name, age } = {
+//   name: "홍길동",
+//   age: 30,
+// };
 
 // const a = 0;
 // const name = '홍길서';
 
+
+interface LoginState {
+  email: string;
+  password: string;
+}
+
 export default function A_useState() {
   const [count, setCount] = useState<number>(0);
+
+  const [loginState, setLoginState] = useState<LoginState>({
+    email: '',
+    password: ''
+  }); 
+
+  const { email, password } = loginState;
 
   // REACT의 체계
   // : TSX문법체계
@@ -45,20 +58,62 @@ export default function A_useState() {
   // 2. HTML 내에서 TS 작성: {} 중괄호로 묶음
 
   const handleIncrementButton = () => {
-    // set 상태설정 함수에 전달되는 인자값으로 count 값이 업데이트 
-    // - 이전의 상태값와 연관이 없는 경우 
+    // set 상태설정 함수에 전달되는 인자값으로 count 값이 업데이트
+    // - 이전의 상태값와 연관이 없는 경우
     // setCount(count + 1);
 
     // - 이전의 상태값과 연관이 있는 경우
     // : prev상태변수명
-    // : 최신의 상태변수값을 가져옴 
-    setCount(prevCount => prevCount + 1);
-  }
+    // : 최신의 상태변수값을 가져옴
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  // 여러 input 창을 관리하는 이벤트 핸들러
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setLoginState((prevState) => ({
+      // 이메일 input창만 이벤트 발생 시 이전의 이메일, 패스워드를 모두 가져와
+      ...prevState, 
+      // 현재 변화가 일어나고 있는 name(email)에 value(입력값)를 넣어 업데이트
+
+      // password는 이전의 값을 그대로 가지고 있음 
+      [name]: value
+    }));
+  };
 
   return (
     <div>
       <p>Count: {count}</p>
       <button onClick={handleIncrementButton}>증가 버튼</button>
+      <hr />
+      <form>
+        <div>
+          <label htmlFor="email">이메일</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={handleInputChange}
+            placeholder="이메일을 입력하세요"
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">패스워드</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            placeholder="패스워드를 입력하세요"
+            required
+          />
+        </div>
+        <button type="submit">로그인</button>
+      </form>
     </div>
-  )
+  );
 }
---
+++ board_front/src/views/ReactStudy/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import UseState from './../../react-study/A_useState';
+
+export default function ReactStudy() {
+  return (
+    <>
+      <UseState />
+    </>
+  )
+}
