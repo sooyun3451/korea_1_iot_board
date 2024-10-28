81c82b959f3dbae6fcfbf321ac6c77f1131ce13b
sooyun3451 <sooyun020820@gmail.com>
2024-10-28 11:49:24 +0900

    frontend project

---
+++ board_front/src/react-study/기초.ts
@@ -0,0 +1,20 @@
+export const tmp = "";
+/* 
+# 단축키.ts 
+
+- 터미널 열기: Ctrl(Commend) + Shift + `(백틱)
+
+- 줄 복사: alt(option) + Shift + 방향키(위/아래)
+- 줄 이동: alt(option) + 방향키(위/아래)
+
+- 들여쓰기: tab
+- 내어쓰기: Shift + tab
+
+- 사이드바 토글: Ctrl(Commend) + b
+
+- 검색: Ctrl(Commend) + f
+
+? - 리액트 함수형 컴포넌트 생성 단축키: rfc/rfce
+
+? - 리액트 실행 명령어: npm run start
+*/
